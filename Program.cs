c031692c28060d3f9e761b7af0ba884c2b9d547f
Jhonatan Silva <jhonatanfdvk@gmail.com>
2024-03-21 22:00:11 -0300

    add interação com o usuario

--- Program.cs
+++ Program.cs
@@ -1,11 +1,45 @@
 ﻿using ExercicioContribuintesPolimorfismo.Entities;
-
+using System.Globalization;
 class Program
 {
     static void Main(string[] args)
     {
-        PessoaJuridica p1 = new PessoaJuridica("Jhonatan", 400000.00, 25);
+        Console.Write("Enter the number of tax payers: ");
+        int quantidadeContribuintes = int.Parse(Console.ReadLine());
 
-        Console.WriteLine(p1.Imposto());
+        List<Pessoa> pessoas = new List<Pessoa>();
+        Pessoa pessoa;
+
+        for (int i = 0; i < quantidadeContribuintes; i++)
+        {
+            Console.WriteLine($"Tax payer #{i + 1} data:");
+            Console.Write("Individual or company (i/c)? ");
+            string choice = Console.ReadLine();
+
+            if (choice == "i")
+            {
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Anual income: ");
+                double ganhoAnual = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Health expenditures: ");
+                double gastosSaude = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                pessoa = new PessoaFisica(name, ganhoAnual, gastosSaude);
+                pessoas.Add(pessoa);
+            }
+            if (choice == "c")
+            {
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Anual income: ");
+                double ganhoAnual = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Number of employees: ");
+                int qtdFuncionarios = int.Parse(Console.ReadLine());
+
+                pessoa = new PessoaJuridica(name, ganhoAnual, qtdFuncionarios);
+                pessoas.Add(pessoa);
+            }
+        }
     }
 }
