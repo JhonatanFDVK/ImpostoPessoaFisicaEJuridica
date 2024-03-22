657eecd22f6c76b40762c7cfccdd3ab6166e809b
Jhonatan Silva <jhonatanfdvk@gmail.com>
2024-03-21 21:44:52 -0300

    Criada a classe principal

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using ExercicioContribuintesPolimorfismo.Entities;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        PessoaJuridica p1 = new PessoaJuridica("Jhonatan", 400000.00, 25);
+
+        Console.WriteLine(p1.Imposto());
+    }
+}
